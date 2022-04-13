96435640f50500d603d39f698043654aab0b3e8b
Charlie Vieth <charlie.vieth@gmail.com>
2022-04-13 16:32:41 -0400

    WIP: attempt to load color schemes from settings
    FIXME: this is broken for some reason

--- switch_theme.py
+++ switch_theme.py
@@ -15,11 +15,13 @@ THEMES = {
 
 
 class SwitchThemeCommand(sublime_plugin.ApplicationCommand):
-    def run(self, theme="Light") -> None:
-        if theme not in THEMES:
-            sublime.error_message(f"invalid theme: {theme}")
+    def run(self, theme_name="Light", light=None, dark=None) -> None:
+        # FIXME: going from Dark to Light sometimes fails to change the Theme
+        # themes = sublime.load_settings("SwitchTheme.sublime-settings").get("themes")
+
+        if theme_name not in THEMES:
+            sublime.error_message(f"invalid theme: {theme_name}")
             return
         s = sublime.load_settings("Preferences.sublime-settings")
-        for key, val in THEMES[theme].items():
-            s.set(key, val)
+        s.update(THEMES[theme_name])
         sublime.save_settings("Preferences.sublime-settings")
